59c0f47f2ac10e0913cf7b7340521790a4d731eb
dubianliao <dubianliao@gmail.com>
2019-02-27 11:52:37 +0900

    edit slider

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ $(function(){
 
 
 });//全体の閉じタグ
+
+var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 20,
+    centeredSlides : true,
+    pagination: '.swiper-pagination',
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    breakpoints: {
+        767: {
+            slidesPerView: 1,
+            spaceBetween: 0
+        }
+    }
+})
